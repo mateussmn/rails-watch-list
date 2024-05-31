ecccc0e19323306ce4731cc3071be267f03e37a1
mateus silva <mateussilvamn18@gmail.com>
2024-05-31 10:32:50 -0300

    watch list2

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -9,12 +9,13 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
     @bookmark.list = @list
+    @bookmark.save
     redirect_to @list
   end
 
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie)
   end
 end
