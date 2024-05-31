20242a3da3d84e18b84a770a1809b123612fe661
mateus silva <mateussilvamn18@gmail.com>
2024-05-31 00:08:57 -0300

    watch list2

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,16 +1,15 @@
 class BookmarksController < ApplicationController
-  before_action :set_list, only: %i[create]
   def new
-    @movie = Movie.find(params[:movie_id])
-    @list = List.find(params[:])
+    @movie = Movie.all.order(:title)
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
     @bookmark.list = @list
-    @bookmark.save
-    redirect_to list_bookmark_path(@list)
+    redirect_to @list
   end
 
   private
@@ -18,8 +17,4 @@ class BookmarksController < ApplicationController
   def bookmark_params
     params.require(:bookmark).permit(:comment)
   end
-
-  def set_list
-    @list = List.find(params[:list_id])
-   end
 end
